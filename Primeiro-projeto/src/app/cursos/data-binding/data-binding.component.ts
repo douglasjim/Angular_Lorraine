fd1a2b7c849f4a62a51c9135efb68e5b717dfef2
Douglasjin <douglas.24tlc@hotmail.com>
2022-03-17 01:32:04 -0300

    Aplicando class e style dinamico eventos de mouse keyup e blur

--- Primeiro-projeto/src/app/cursos/data-binding/data-binding.component.ts
+++ Primeiro-projeto/src/app/cursos/data-binding/data-binding.component.ts
@@ -9,12 +9,18 @@ export class DataBindingComponent implements OnInit {
 
   url: string = "http:www.teste.com.br";
   img: string;
+  //podemos iniciar a variavel vazia ou nao, mas sempre bom colocar o tipo dela
+  valorAtualInput: string = '';
+  valorSalvo;
+  isMouseOver;
 
   constructor() { }
 
   ngOnInit(): void {
     this.img = "http://lorempixel.com.br/200/200/?1"
   }
+
+
 
   result(){
     return 10
@@ -24,6 +30,31 @@ export class DataBindingComponent implements OnInit {
     console.log("cliente adicionado")
   }
 
-  
+  onKeyUp(evento){
+    console.log("onKeyUpChamado", evento.target.value);
+    this.valorAtualInput = evento.target.value;
+    console.log("this.valorAtualInput" + this.valorAtualInput)
+  }
+
+  onKeyUpEnter(valor){
+    this.valorSalvo = valor;
+    console.log("valorSalvo" , this.valorSalvo)
+  }
+
+  //a funcao blur no input serve para quando o campo input perder o foco a funcao blur é chamada
+  inputBlur(valorBlur){
+    console.log("valorBlur" , valorBlur)
+  }
+
+  onKeyUpEnterVariavelDaView(valorDaView){
+    console.log("valorDaView", valorDaView)
+  }
+
+  onMouseOver(){
+    this.isMouseOver = !this.isMouseOver;
+    console.log("isMouseOver" + this.isMouseOver)
+  }
+
+
 
 }
